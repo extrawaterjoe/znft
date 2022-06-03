0334b931483f76c0659f5e6200e3dc3f0700b294
joe lazarz <jlazarz87@gmail.com>
2022-06-03 12:31:21 -0400

    stiches config

--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -1,10 +1,32 @@
 import React from "react"
 import Nft from "./Nft"
+import { styled } from "stitches.config"
+
+const Text = styled("p", {
+  fontFamily: "$system",
+  color: "blue",
+
+  variants: {
+    size: {
+      1: {
+        fontSize: "$1",
+      },
+      2: {
+        fontSize: "$2",
+      },
+      3: {
+        fontSize: "$3",
+      },
+    },
+  },
+})
 
 const Gallery = () => {
   return (
     <div>
-      <h1>Gallery</h1>
+      <Text as="h1" size="3">
+        Gallery
+      </Text>
       <div>
         <Nft />
       </div>
